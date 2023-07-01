860107ee70c8c0b33fce4ff39918311870413865
Maheshkumar0018 <maheshkumar7415.com>
2023-07-01 22:11:55 +0530

    updated

--- static/js/sidebar.js
+++ static/js/sidebar.js
@@ -1,12 +1,17 @@
 /* Open the sidebar */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.querySelector(".sidebar").classList.add("open-nav");
-  }
-  
-  /* Close the sidebar */
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.querySelector(".sidebar").classList.remove("open-nav");
-  }
-  
+  document.getElementById("mySidenav").style.width = "250px";
+  document.querySelector(".sidebar").classList.add("open-nav");
+  document.querySelector(".container").style.marginLeft = "250px";
+  document.querySelector(".box1").style.backgroundColor = "transparent";
+  document.querySelector(".box2").style.marginLeft = "250px";
+}
+
+/* Close the sidebar */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.querySelector(".sidebar").classList.remove("open-nav");
+  document.querySelector(".container").style.marginLeft = "0";
+  document.querySelector(".box1").style.backgroundColor = "#F8F8F8";
+  document.querySelector(".box2").style.marginLeft = "0";
+}
